6c62048ecc68d9bb4527e71aa3c918ecf8c3fc50
maddanarajeshkumar <rajesh.m476@gmail.com>
2018-04-09 18:10:32 +0530

    update

--- src/day1/StoreArray.java
+++ src/day1/StoreArray.java
@@ -1,9 +1,62 @@
 package day1;
-
-public class StoreArray {
-	public void Input()
+import java.util.*;
+import java.io.IOException;
+import java.util.Arrays;
+public class StoreArray 
+{
+	
+	int []a;
+	StoreArray()
 	{
-	int a[]=new int[5];
+		a=new int[10];
 	}
+	StoreArray(int x)
+	{
+		a=new int[x];
+	}
+	StoreArray(int x[])
+	{
+		a=x;
+	}
+	StoreArray(StoreArray x)
+	{
+		this.a=x.a;
+	}
+	void read() throws NumberFormatException,IOException
+	{
+		System.out.println("enter"+a.length+"elements");
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=Integer.parseInt(read.input.readLine());
+		}
+	}
+	void display()
+	{
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a+" ");
+		}
+		System.out.println("");
+	}
+	void sort1()
+	{
+		Arrays.sort(a);
+	}
+	void search(int key)
+	{
+		sort1();
+		int k=Arrays.binarySearch(a,key);
+		if(k<0)
+		{
+			System.out.println("Element not found");
+		}
+		else
+			System.out.println("Element found"+k);
+	}
+	public static void main(String []args)throws IOException
+	{
+		
+	}
+	
 	
 }
