443921c40efbcead03ae82800171d68c3c1a248b
maddanarajeshkumar <rajesh.m476@gmail.com>
2018-04-09 15:18:18 +0530

    update

--- src/day1/Constructor.java
+++ src/day1/Constructor.java
@@ -4,7 +4,11 @@ public class Constructor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		dem ob=new dem();
+		ob.f1();
+		System.out.println("...............");
+		dem ob2=new dem();
+		ob2.f1();
 	}
 
 }
--- src/day1/dem.java
+++ src/day1/dem.java
@@ -8,8 +8,22 @@ public class dem {
 	dem(int e){
 		System.out.println(e);
 	}
+	/*A constructor can't be a
+	 * static but it can be private
+	 */
 	dem(){
 		this(2);
 		System.out.println("i am in 0 arg constructor");
 	}
+	{
+		System.out.println("i am in 1 annonymous block");
+		f1();
+	}
+	static
+	{
+		System.out.println("i am in 2 annonymous block");
+	}
+	/*Static can be executed only once
+	 * before calling methods all constructor should be executed
+	 */
 }
