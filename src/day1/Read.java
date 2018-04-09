0c0429ea61088ef46091252df9d604d85a06ff95
maddanarajeshkumar <rajesh.m476@gmail.com>
2018-04-09 14:45:26 +0530

    updated

---
+++ src/day1/Read.java
@@ -0,0 +1,6 @@
+package day1;
+import java.util.*;
+public class Read {
+	static Scanner sc=new Scanner(System.in);
+
+}
---
+++ src/day1/de.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class de {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		System.out.println("enter the number");
+		a=Read.sc.nextInt();
+		System.out.println(a);
+
+	}
+
+}
