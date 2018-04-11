38efc91f055a480659370f0d1a887743cd4d2ef4
maddanarajeshkumar <rajesh.m476@gmail.com>
2018-04-11 09:42:28 +0530

    okay

--- src/day2/Planetmain.java
+++ src/day2/Planetmain.java
@@ -9,7 +9,39 @@ public class Planetmain {
 		System.out.println(ob);
 		System.out.println(ob.volume+" g/cm3");
 		System.out.println(ob.seconds+" seconds");
-
+		ob=Planet.valueOf("venus");
+		System.out.println(ob);
+		System.out.println(ob.volume+" g/cm3");
+		System.out.println(ob.seconds+" seconds");
+		ob=Planet.valueOf("earth");
+		System.out.println(ob);
+		System.out.println(ob.volume+" g/cm3");
+		System.out.println(ob.seconds+" seconds");
+		ob=Planet.valueOf("mars");
+		System.out.println(ob);
+		System.out.println(ob.volume+" g/cm3");
+		System.out.println(ob.seconds+" seconds");
+		ob=Planet.valueOf("jupiter");
+		System.out.println(ob);
+		System.out.println(ob.volume+" g/cm3");
+		System.out.println(ob.seconds+" seconds");
+		ob=Planet.valueOf("saturn");
+		System.out.println(ob);
+		System.out.println(ob.volume+" g/cm3");
+		System.out.println(ob.seconds+" seconds");
+		ob=Planet.valueOf("uranus");
+		System.out.println(ob);
+		System.out.println(ob.volume+" g/cm3");
+		System.out.println(ob.seconds+" seconds");
+		ob=Planet.valueOf("neptune");
+		System.out.println(ob);
+		System.out.println(ob.volume+" g/cm3");
+		System.out.println(ob.seconds+" seconds");
+		ob=Planet.valueOf("pluto");
+		System.out.println(ob);
+		System.out.println(ob.volume+" g/cm3");
+		System.out.println(ob.seconds+" seconds");
+		
 
 	}
 
